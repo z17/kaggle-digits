063ae09fc3dab2350168f3fdc0fcdc3b86497361
Maksim Iliukovich <m.iliukovich@corp.vk.com>
2017-12-27 16:55:12 +0300

    second solution

--- src/functions.py
+++ src/functions.py
@@ -1,9 +1,19 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
-def showimage(i, train_images):
-    img = train_images.iloc[i].as_matrix()
+def showimage(i, images):
+    img = images.iloc[i].as_matrix()
     img = img.reshape((28, 28))
     plt.imshow(img, cmap='binary')
     plt.show()
+    return
+
+
+def save_results(results, path):
+    df = pd.DataFrame(results)
+    df.index += 1
+    df.index.name = 'ImageId'
+    df.columns = ['Label']
+    df.to_csv(path, header=True)
     return
--- src/svc.py
+++ src/svc.py
@@ -13,7 +13,7 @@ train_images, test_images, train_labels, test_labels = train_test_split(images, 
 test_images[test_images > 0] = 1
 train_images[train_images > 0] = 1
 
-clf = svm.SVC()
+clf = svm.SVC(gamma=0.001, C=100)
 clf.fit(train_images, train_labels.values.ravel())
 print clf.score(test_images, test_labels)
 
@@ -21,8 +21,4 @@ test_data = pd.read_csv('../data/test.csv')
 test_data[test_data > 0] = 1
 results = clf.predict(test_data)
 
-df = pd.DataFrame(results)
-df.index += 1
-df.index.name = 'ImageId'
-df.columns = ['Label']
-df.to_csv('results.csv', header=True)
+functions.save_results(results, '../data/results.csv')
